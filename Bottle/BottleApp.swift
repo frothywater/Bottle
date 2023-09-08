ba87b646e9acfc3c13036ab571eac51c1db886eb
frothywater <kokoroic@outlook.com>
2023-09-08 11:23:29 +0800

    Initial Commit

---
+++ Bottle/BottleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottleApp.swift
+//  Bottle
+//
+//  Created by Cobalt on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BottleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
